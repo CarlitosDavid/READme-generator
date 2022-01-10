cf6c08569e79770c38933b92fca9859faca09fc1
Carl Contreras <contrerascarl09@gmail.com>
2022-01-09 22:34:09 -0600

    starter code for question prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,87 +1,66 @@
-// TODO: Include packages needed for this application
-const inquier = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-const api = require('./utils/api.js');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
-        message: "What is you GitHub Username?"
-        name: 'username',
-        default: 'connietran-dev',
-        validate: function (answer) {
-            if (answer.lenght < 1) {
-                return console.log("A valid GitHub username is required.");
-            }
-            return true; 
-        }
+        message: "What is the title of your project?",
+        name: "title"
     },
     {
-        type: 'input',
-        message: "What is the name of the GitHub repo?",
-        name: 'repo', 
-        default: 'readme-generator',
-        validate: function (answer) {
-            if (answer.lenght < 1) {
-                return console.log ("A valid GitHub repo is required for a badge.");
-            }
-            return true;
-        }
+        type: "input", 
+        message: "what is the project about?",
+        name: "Description"
     },
     {
-        type: 'input',
-        message: "Write a description of your project.", 
-        name: 'description',
-        default: 'Project Description',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid project description is required.");
-            }
-            return true;
-        }
+        type: "input",
+        message: "What does the user need to install this app?",
+        name: "Installation"
     },
     {
-        type: 'input',
-        message: "If applicable, describe the steps required to install your project for the Install section.",
-        name: 'installation',
+        type: "input",
+        message: "What liscence is being used?",
+        name: "License"
     },
     {
-        type: 'input',
-        message: "if applicable, provide guidelines on how other developers can contribute to your project.",
-        name: 'contributing'
+        type: "input", 
+        message: "What commands are used to run this app?",
+        name: "commands"
     },
     {
-        type: 'input',
-        message: "if applicable, provdie any test written for your application and provide example on how to run them.",
-        name: 'tests'
+        type: "input",
+        message: "what is your Github username?",
+        name: "Username"
     },
     {
-        type: 'list',
-        message: "Choose a license for your project.", 
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
+        type: "input",
+        message: "what is your email address?"
+        name: "Email"
     },
-];
+]
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
 
-        console.log("Success! Your README.md file has been generated")
-    });
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("success")
+        }
+    })
 }
 
-const writeFileAsync = util.promisify(writeToFile); 
+function init() {
+    inquirer.prompt(questions)
+    .then(funciton(data) {
+        writeFile("README.md", generatorMarkdown(data));
+        console.log(data)
+    })
+}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
